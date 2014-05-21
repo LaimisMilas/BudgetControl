d932a89918aa2c2535f9ddbb01032078cdd52313
Nickolay Miroshnychenko <nsmirosh@gmail.com>
2014-05-21 10:46:32 +0300

    added the basic layout for the budgetSettings activity

--- app/src/main/java/nick/miros/BudgetControl/budgetcontrol/app/BudgetSettings.java
+++ app/src/main/java/nick/miros/BudgetControl/budgetcontrol/app/BudgetSettings.java
@@ -4,14 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class BudgetSettings extends ActionBarActivity {
+
+    TextView currentMonth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_settings);
+
+        currentMonth = (TextView) findViewById(R.id.currentMonth);
+
     }
 
 
