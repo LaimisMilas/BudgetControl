1fcd8e14f0e23d8efb527378a6cb6ccfb42b880c
Nickolay Miroshnychenko <nsmirosh@gmail.com>
2014-05-21 23:52:24 +0300

    implemented the AlertDialogue for setting the monthly budget

--- app/src/main/java/nick/miros/BudgetControl/budgetcontrol/app/BudgetSettings.java
+++ app/src/main/java/nick/miros/BudgetControl/budgetcontrol/app/BudgetSettings.java
@@ -1,9 +1,17 @@
 package nick.miros.BudgetControl.budgetcontrol.app;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -12,10 +20,13 @@ import java.util.Calendar;
 public class BudgetSettings extends ActionBarActivity {
 
     TextView currentMonth;
+    final Context context = this;
+    private ImageButton BudgetEditButton;
+    private TextView MonthlyBudget;
 
-    String[] monthNames = { "January", "February", "March", "April", "May",
+    String[] monthNames = {"January", "February", "March", "April", "May",
             "June", "July", "August", "September", "October", "November",
-            "December" };
+            "December"};
 
 
     @Override
@@ -29,6 +40,58 @@ public class BudgetSettings extends ActionBarActivity {
         int month = cal.get(Calendar.MONTH);
 
         currentMonth.setText("Budget for " + monthNames[month]);
+
+
+        BudgetEditButton = (ImageButton) findViewById(R.id.EditButton);
+        MonthlyBudget = (TextView) findViewById(R.id.currentMonthBudget);
+
+        // add button listener
+        BudgetEditButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.budget_prompt, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        MonthlyBudget.setText(userInput.getText());
+                                    }
+                                }
+                        )
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                }
+                        );
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
 
 
     }
