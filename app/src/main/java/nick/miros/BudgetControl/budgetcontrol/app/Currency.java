b83090cc40235b9c76f9760c0a44a8b7b634f70b
Nickolay Miroshnychenko <nsmirosh@gmail.com>
2014-06-05 21:20:38 +0300

    created the currency class
    I created a currency class with 3 fields -
    country, currencyName and symbol for further
    usage in an adapter in currencyActivity

---
+++ app/src/main/java/nick/miros/BudgetControl/budgetcontrol/app/Currency.java
@@ -0,0 +1,39 @@
+package nick.miros.BudgetControl.budgetcontrol.app;
+
+/**
+ * Created by mymi on 05-Jun-14.
+ */
+public class Currency {
+    private String country;
+    private String currencyName;
+    private String symbol;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCurrencyName() {
+        return currencyName;
+    }
+
+    public void setCurrencyName(String currencyName) {
+        this.currencyName = currencyName;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public String toString() {
+        return country + " " + currencyName + " " + symbol;
+    }
+}
