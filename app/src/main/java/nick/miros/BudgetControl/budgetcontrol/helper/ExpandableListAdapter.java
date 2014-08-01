675ee2172d7592832676ea2f1bebb4944d449109
Nick Miroshnychenko <nsmirosh@gmail.com>
2014-07-31 22:17:45 -0400

    Revert "Add two sliding buttons to each item"
    This reverts commit d941d4d7c95f91cf2bb3d6240461aef163c094b3.
    
    Decided that it would be too much trouble dealing with this
    toolbar below each expandable item. Decided to make a separate
    activity that will pop-up upon clicking on the item of the
    expandable list view.
    
    - revert the commit that makes the toolbar with edit and delete
    buttons
    - remove all of animation classes and everything else that went
    with it.

--- app/src/main/java/nick/miros/BudgetControl/budgetcontrol/helper/ExpandableListAdapter.java
+++ app/src/main/java/nick/miros/BudgetControl/budgetcontrol/helper/ExpandableListAdapter.java
@@ -9,13 +9,8 @@ import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -47,7 +42,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter{
     }
 
     @Override
-    public View getChildView(final int groupPosition, final int childPosition,
+    public View getChildView(int groupPosition, final int childPosition,
                              boolean isLastChild, View convertView, ViewGroup parent) {
 
         if (convertView == null) {
@@ -63,29 +58,6 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter{
 
         groupChildDescription.setText(_sortedExpenses.get(groupPosition).get(childPosition).getDescription() + "");
         groupChildAmount.setText(_sortedExpenses.get(groupPosition).get(childPosition).getAmount() + "");
-
-        final View toolbar = convertView.findViewById(R.id.toolbar);
-
-        // Resets the toolbar to be closed
-        ((LinearLayout.LayoutParams) toolbar.getLayoutParams()).bottomMargin = -50;
-        toolbar.setVisibility(View.GONE);
-
-        convertView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(_context, _sortedExpenses.get(groupPosition).get(childPosition).getDescription(),
-                        Toast.LENGTH_SHORT).show();
-
-                View toolbar = view.findViewById(R.id.toolbar);
-
-                // Creating the expand animation for the item
-                ExpandAnimation expandAni = new ExpandAnimation(toolbar, 300);
-
-                // Start the animation on the toolbar
-                toolbar.startAnimation(expandAni);
-            }
-        });
-
         return convertView;
     }
 
@@ -148,7 +120,4 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter{
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
     }
-
-
-
 }
